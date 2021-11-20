ad97044ee6fb3f98518392e8e111fb7e0310becb
sabuj7177 <laskar7177sabuj@gmail.com>
2021-11-20 08:15:25 +0600

    added cifar100 test for pytorch

---
+++ test.py
@@ -0,0 +1,102 @@
+#test.py
+#!/usr/bin/env python3
+
+""" test neuron network performace
+print top1 and top5 err on test dataset
+of a model
+
+author baiyu
+"""
+
+import argparse
+from collections import defaultdict
+
+from matplotlib import pyplot as plt
+
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+from conf import settings
+from utils import get_network, get_test_dataloader
+
+label_names = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
+d = defaultdict(dict)
+
+def get_density(original_index, predicted_index):
+    for i in range(len(original_index)):
+        org_val = label_names[original_index[i]]
+        pred_val = label_names[predicted_index[i]]
+        if org_val != pred_val:
+            if (org_val in d) & (pred_val in d[org_val]):
+                d[org_val][pred_val] += 1
+            else:
+                d[org_val][pred_val] = 1
+
+
+def print_labels_from_index(label_index):
+    for index in label_index:
+        print(label_names[index])
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-net', type=str, required=True, help='net type')
+    parser.add_argument('-weights', type=str, required=True, help='the weights file you want to test')
+    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('-b', type=int, default=16, help='batch size for dataloader')
+    args = parser.parse_args()
+
+    net = get_network(args)
+
+    cifar100_test_loader = get_test_dataloader(
+        settings.CIFAR100_TRAIN_MEAN,
+        settings.CIFAR100_TRAIN_STD,
+        #settings.CIFAR100_PATH,
+        num_workers=4,
+        batch_size=args.b,
+    )
+
+    net.load_state_dict(torch.load(args.weights))
+    print(net)
+    net.eval()
+
+    correct_1 = 0.0
+    correct_5 = 0.0
+    total = 0
+
+    with torch.no_grad():
+        for n_iter, (image, label) in enumerate(cifar100_test_loader):
+            print("iteration: {}\ttotal {} iterations".format(n_iter + 1, len(cifar100_test_loader)))
+
+            if args.gpu:
+                image = image.cuda()
+                label = label.cuda()
+                print('GPU INFO.....')
+                print(torch.cuda.memory_summary(), end='')
+
+
+            output = net(image)
+            _, pred = output.topk(1, 1, largest=True, sorted=True)
+
+            label = label.view(label.size(0), -1).expand_as(pred)
+            get_density(label.flatten().tolist(), pred.flatten().tolist())
+            # print(label.flatten().tolist())
+            # print(pred.flatten().tolist())
+            correct = pred.eq(label).float()
+
+            #compute top 5
+            correct_5 += correct[:, :5].sum()
+
+            #compute top1
+            correct_1 += correct[:, :1].sum()
+
+    if args.gpu:
+        print('GPU INFO.....')
+        print(torch.cuda.memory_summary(), end='')
+
+    print()
+    print("Top 1 err: ", 1 - correct_1 / len(cifar100_test_loader.dataset))
+    print("Top 5 err: ", 1 - correct_5 / len(cifar100_test_loader.dataset))
+    print("Parameter numbers: {}".format(sum(p.numel() for p in net.parameters())))
+    print(d)
